01806c5556a6ab0ef3389deec109ba6509fa9391
Krishna-R-Sonar <krishnarsonar@gmail.com>
2025-04-27 19:04:18 +0530

    chore: Clean up import statements in index.js for consistency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Filename: src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use react-dom/client for React 18
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import './App.css'; // Import your CSS file
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
