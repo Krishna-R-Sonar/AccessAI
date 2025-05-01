29ae45f5db04e020a20af094bb97972e2b84524b
Krishna-R-Sonar <krishnarsonar@gmail.com>
2025-05-02 00:52:52 +0530

    refactor: Simplify MongoDB connection options in server.js

--- server/server.js
+++ server/server.js
@@ -12,10 +12,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
- useNewUrlParser: true,
- useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log('Connected to MongoDB'))
  .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware
