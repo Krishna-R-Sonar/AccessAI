cdc92a9a768bfe48ef4a7bb748fb20bea7a48005
Krishna-R-Sonar <krishnarsonar@gmail.com>
2025-09-24 23:06:18 +0530

    fix: Update stylesheet import from styles.css to App.css

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './styles.css';
+import './App.css';
 
 function Signup({ setUser }) {
   const [email, setEmail] = useState('');
